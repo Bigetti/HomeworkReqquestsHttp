6417b63ca3c5c5d07db0c3b9bf7f12e93ddfb441
bigetti <upgrade81@mail.ru>
2023-07-13 17:13:05 +0300

    Попытка 3 задание 2

--- yandexDiskUpload.py
+++ yandexDiskUpload.py
@@ -1,78 +1,46 @@
 import requests
-import pickle
-
-import json
+import os
 
 token = open("token").read()
-
-dict_responce ={}
 
 class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
     def upload(self, file_path: str):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
-        # Тут ваша логика
-
-        # Не совсем поимаю как я должен был понять что путь именно такой, ГПТ чат.
         url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = {"Authorization": f"OAuth {self.token}"}
         
-        #Только благодаря ГПТ чату понял ято понадобится имя файла и как его добыть
-        file_name = path_to_file.split('\\')[-1]
+        file_name = os.path.basename(file_path)
+        target_url = f'{url}?path={file_name}&overwrite=true'
 
         with open(file_path, "rb") as file:
-            data = file.read()
-
-       
-        # Отправляем запрос на загрузку файла, это ГПТ чат, сам я не понимаю как такой код писать
-        response = requests.put(
-            url,
-            headers=headers,
-            params={"path": file_name},
-            data=data
-        )
-
-        if response.status_code == 201:
-            print("File was successfully aploaded to YandexDisk")
-            # Извлекаем ссылку на загруженный файл
-            upload_info = response.json()
-            file_link = upload_info.get("href")
-            if file_link:
-                print("Ссылка на загруженный файл:", file_link)
+            response = requests.get(target_url, headers=headers)
+            
+            if response.status_code == 200:
+                upload_info = response.json()
+                upload_url = upload_info.get("href")
+                if upload_url:
+                    with open(file_path, "rb") as file:
+                        response = requests.put(upload_url, headers=headers, data=file)
+                        print("Upload Response Status Code:", response.status_code)
+                        print("Upload Response Content:", response.text)
+                        if response.status_code == 201:
+                            print("Successfully uploaded")
+                            file_link = response.json().get("href")
+                            if file_link:
+                                print("Ссылка на загруженный файл:", file_link)
+                            else:
+                                print("Не удалось получить ссылку на загруженный файл")
+                        else:
+                            print("Произошла ошибка при загрузке файла")
+                else:
+                    print("Не удалось получить URL-адрес для загрузки файла")
             else:
-                print("Не удалось получить ссылку на загруженный файл")
-        else:
-            print("Mistake happend")
-
-        dict_responce = response.json()
-
-
-        #Хочу выгрузить данные в файл  и посмотреть что там, но получаю какую-то ошибку внутри файла, сама выгрузка идет
-        with open("file.txt", 'w') as f:
-            json.dump(dict_responce, f, ensure_ascii=False, indent=2)
-        
-        print(type(dict_responce))
-        #убеждаюсь, что это словарь
-
-        return dict_responce
-    
-    def __str__(self):
-        return json.dumps(self.dict_responce, indent=4)
-        
-
-   
-        
-
+                print("Произошла ошибка при получении URL-адреса для загрузки файла")
 
 if __name__ == '__main__':
-    # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = 'F:\\NETOLOGYPython\\HomeworkReqquestsHttp\\forUpload.txt'
+    path_to_file = r'F:\NETOLOGYPython\HomeworkReqquestsHttp\forUpload.txt'
     token = token
     uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
-    
-    
-    print(result)
-    
+    uploader.upload(path_to_file)
